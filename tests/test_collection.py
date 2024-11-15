9ae67017346e6afc141b74d4d00e323c5f0d622b
sherlyn99 <y1weng@ucsd.edu>
2024-11-15 13:52:18 -0800

    run collection tests.

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -8,16 +8,28 @@ from wipe.modules.collection import (
 
 class CollectionTests(unittest.TestCase):
     def test_compile_results_checkm2(self):
-        pass
+        obs = compile_results_checkm2(
+            "/projects/greengenes2/gg2_genomes/linearized_test/G"
+        )
+        obs.to_csv(
+            "./tests/tmp/checkm2.tsv", index=False, header=True, sep="\t"
+        )
 
-    # def test_compile_results_prodigal(self):
-    #     obs = compile_results_prodigal(
-    #         "/projects/greengenes2/gg2_genomes/linearized_test/G"
-    #     )
-    #     print(obs.head(5))
+    def test_compile_results_prodigal(self):
+        obs = compile_results_prodigal(
+            "/projects/greengenes2/gg2_genomes/linearized_test/G"
+        )
+        obs.to_csv(
+            "./tests/tmp/proteins.tsv", index=False, header=True, sep="\t"
+        )
 
-    # def test_generate_coords(self):
-    #     generate_coords(
-    #         "/projects/greengenes2/gg2_genomes/linearized_test/G/964/307",
-    #         "./tests/tmp/coords.txt.xz",
-    #     )
+    def test_generate_coords(self):
+        generate_coords(
+            "/projects/greengenes2/gg2_genomes/linearized_test/G/964/307",
+            "./tests/tmp/coords.txt.xz",
+        )
+
+
+if __name__ == "__main__":
+    pass
+    # unittest.main()
